c024ba40f17a9d9588c74dd81662d07475acb515
lugamecooper <legamerfdp81@gmail.com>
2024-12-05 23:39:55 +0100

    .

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <html>
     <head>
         <title>oceanQT</title>
-        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="./src/images/.ico">
+        <link rel="stylesheet" href="./style.css">
     </head>
+    <body>
+        <h1>
+            La mèr, plus qu'une grande flaque d'eau c'est une chose étonament complexe, aussi complexe que le corps humain, 
+            <br>
+            l'on découvrira que l'on peut comparer les plus grands océan à nos "simples" corps d'être humain et ainsi nous permetrent de nous rendre compte du lien qui nous unis à elle.
+            
+        </h1>
+    </body>
 </html>
