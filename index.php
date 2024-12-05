7f46dcc9c9c01863fe799e31872ffa101d26f1c0
manu321-code <Emmanuel.tchertchian.32@gmail.com>
2024-12-05 23:53:29 +0100

    .

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="mystyle.css">
     <title>Document</title>
 </head>
 <body>
     <?php include 'header.php';?>
 
-    <?php include 'footer.php';?>
+    <?php include("footer.php");?>
+
 </body>
 </html>
