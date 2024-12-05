e1f79e935421d1dcc4763aa909924c3dcd35cf80
lugamecooper <legamerfdp81@gmail.com>
2024-12-05 23:58:22 +0100

    .

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
         <link rel="stylesheet" href="./style.css">
     </head>
     <body>
+        <div class="div_martyr">
         <h1>
             La mèr, plus qu'une grande flaque d'eau c'est une chose étonament complexe, aussi complexe que le corps humain, 
             <br>
             l'on découvrira que l'on peut comparer les plus grands océan à nos "simples" corps d'être humain et ainsi nous permetrent de nous rendre compte du lien qui nous unis à elle.
-            
         </h1>
+        </div>
     </body>
 </html>
