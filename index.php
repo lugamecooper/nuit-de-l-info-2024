4e83d1ac9bed93cf6e1e80b0e87a4d187ce46fa5
lugamecooper <legamerfdp81@gmail.com>
2024-12-06 03:14:36 +0100

    .

--- index.php
+++ index.php
@@ -17,9 +17,12 @@
         <br>
         <br>
         <img style="margin-left: 500px; z-index: -1; float:left" height="1500px" width="900" src="./src/images/corpscontour-removebg-preview.png">
-        <img style="position:absolute; left:850px" src="./src/images/virus.png" id="virus">
-        <img style="position:absolute; left:825px; top:550px; max-width: 100px; max-height: 100px;" src="./src/images/coeur.png" id="coeur">
+        <img style="position:absolute; left:900px; top:225px; max-width: 90px; max-height: 90px;" src="./src/images/virus.png" id="virus">
+        <img style="position:absolute; left:825px; top:550px; max-width: 90px; max-height: 90px;" src="./src/images/coeur.png" id="coeur">
         <img style="position:absolute; left:900px; top:650px; max-width: 100px; max-height: 100px;" src="./src/images/estomac.png" id="estomac">
         <img style="position:absolute; left:850px; top:370px; max-width: 200px; max-height: 200px;" src="./src/images/poumons.png" id="poumons">
+        <img style="position:absolute; left:770px; top:870px; max-width: 200px; max-height: 200px;" src="./src/images/veine.png" id="veine">
+        <img style="position:absolute; left:970px; top:870px; max-width: 200px; height: 320px;" src="./src/images/squelettejambe.png" id="squelettejambe">
+        <img style="position:absolute; left:970px; top:770px; max-width: 100px; height: 100px;" src="./src/images/cell.png" id="cell">
     </body>
 </html>
